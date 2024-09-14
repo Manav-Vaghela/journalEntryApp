5306d3aa6aae716dba41a13404e38b25c87f11bb
Manav <vaghelamanav2320@gmail.com>
2024-09-14 17:54:17 +0530

    Moved apikey to application properties

--- src/main/java/com/manav/journalEntryApp/service/JournalEntryService.java
+++ src/main/java/com/manav/journalEntryApp/service/JournalEntryService.java
@@ -6,12 +6,13 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import com.manav.journalEntryApp.entity.User;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Component
+@Service
 public class JournalEntryService{
 
     @Autowired
--- src/main/java/com/manav/journalEntryApp/service/WeatherService.java
+++ src/main/java/com/manav/journalEntryApp/service/WeatherService.java
@@ -2,16 +2,19 @@ package com.manav.journalEntryApp.service;
 
 import com.manav.journalEntryApp.Api_Response.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.client.RestTemplate;
 
-@Component
+@Service
 public class WeatherService {
 
-    private static final String apikey = "Your generated API Key from Weather Stack";
+    @Value("${weather.api.key}")
+    private String apikey;
 
     private static final String API = "http://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
 
